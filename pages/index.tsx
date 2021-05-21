3dd7d7e655bc6a3b631f5f806e0a890f2a7bdac2
endureconsulting <78554883+endureconsulting@users.noreply.github.com>
2021-05-21 17:33:59 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ export default function Home() {
         </div>
       </main>
       <Footer>
-        &copy; {new Date().getFullYear()} | Endure Consulting, LLC
+        &copy; {new Date().getFullYear()} | Endure Consulting LLC
       </Footer>
     </Container>
   );
