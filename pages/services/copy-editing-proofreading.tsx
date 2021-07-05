adfa5daf9143a6842581cda76359b4f7f97c1a75
endureconsulting <78554883+endureconsulting@users.noreply.github.com>
2021-07-05 16:34:47 -0500

    Update copy-editing-proofreading.tsx

--- pages/services/copy-editing-proofreading.tsx
+++ pages/services/copy-editing-proofreading.tsx
@@ -30,8 +30,7 @@ export default function CopyEditingProofreadingServices() {
             </div>
             <ul className="mt-3 ml-5 sm:ml-0 list-circle">
               <li>
-                Ensure all material has correct grammar, punctuation, and
-                spelling.
+                Ensure all material has correct spelling, grammar and punctuation.
               </li>
               <li>Ensure consistencies in formatting and styling.</li>
               <li>
