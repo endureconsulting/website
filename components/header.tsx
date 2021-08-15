0f6095807f069303f0b048e48b44be3edd352c3b
Brent Mealhouse <bmealhouse@gmail.com>
2021-08-15 08:34:37 -0500

    initial commit

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const Header = ({ children, headline, subhead }: Props) => (
   <div className="mt-16 pt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
     <div className="text-center">
-      <h1 className="text-5xl tracking-tight font-extrabold text-gray-900 xs:text-6xl md:text-7xl">
+      <h1 className="text-5xl tracking-tight font-extrabold text-gray-900 xs:text-6xl md:text-2xl">
         {headline}
       </h1>
       <h2 className="mt-1 text-2xl tracking-tighter font-light text-gray-900 xs:text-3xl md:text-4xl">
