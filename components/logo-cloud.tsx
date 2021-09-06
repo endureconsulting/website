7f9ddf891928ac7cdb917b1389f7c23f7ab38886
Brent Mealhouse <bmealhouse@gmail.com>
2021-09-05 19:11:46 -0500

    Increase logo size in logo cloud

--- components/logo-cloud.tsx
+++ components/logo-cloud.tsx
@@ -12,7 +12,7 @@ export const LogoCloud = ({
 }: PropsWithChildren<LogoCloudProps>) => (
   <Cta>
     <Cta.Title>{title}</Cta.Title>
-    <div className="mt-6 grid grid-cols-1 gap-0.5 sm:grid-cols-2 md:grid-cols-3 lg:mt-8">
+    <div className="mt-6 grid grid-cols-1 gap-0.5 sm:grid-cols-2 md:grid-cols-2 lg:mt-8">
       {children}
     </div>
   </Cta>
